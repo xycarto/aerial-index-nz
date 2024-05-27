5b5c86dabe163bd040b0994b37db29a8132a9f93
xycarto <ian@xycarto.com>
2024-05-27 18:04:18 +1200

    delete old merged file

--- src/merge-region.sh
+++ src/merge-region.sh
@@ -5,4 +5,8 @@ REGION=$1
 region_gpkgs=data/${REGION}
 merged_gpkg=${region_gpkgs}/${REGION}.gpkg
 
+if [[ -f $merged_gpkg ]]; then
+    rm $merged_gpkg
+fi
+
 ogrmerge.py -single -overwrite_ds -f GPKG -o ${merged_gpkg} ${region_gpkgs}/*.gpkg
